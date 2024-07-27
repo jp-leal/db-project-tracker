ab1487bcae06f0a4c0461d1512a62b7079fd5312
joaobug <joaoenfrance@gmail.com>
2024-07-27 12:14:31 -0300

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Joao Leal on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
